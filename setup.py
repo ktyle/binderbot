db3a95f943f023f6ca68f14d835a4f7574440b5f
Kevin Tyle <ktyle@albany.edu>
2025-01-30 15:01:01 -0500

    Remove versioneer and update supported Python vers

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
-import versioneer
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
@@ -15,19 +14,19 @@ setup_requirements = ['pytest-runner', ]
 test_requirements = ['pytest>=3', ]
 
 setup(
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     author="Ryan Abernathey",
     author_email='ryan.abernathey@gmail.com',
-    python_requires='>=3.7',
+    python_requires='>=3.10',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
     ],
     description="A simple CLI to interact with binder.",
     entry_points={
@@ -43,8 +42,6 @@ setup(
         'nbformat',
         'nbconvert'
     ],
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='binderbot',
     name='binderbot',
